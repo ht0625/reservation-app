82e0b2092cdc9adc1e4ff01af22bd389922c2653
ht0625 <hiirookii0625@gmail.com>
2021-03-10 16:37:59 +0900

    product-detailコンポーネント追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: ProductListComponent },
+  { path: "detail", component: ProductDetailComponent },
+  // { path: "products/:productId", component: ProductDetailComponent },
+  // { path: "cart", component: CartComponent },
+  // { path: "shipping", component: ShippingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
