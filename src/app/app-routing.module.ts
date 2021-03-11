b6b0ef6f1266201293b597e4997cb2927431cc19
hiroki_tamura@gio-tech.jp <hiroki_tamura@gio-tech.jp>
2021-03-11 11:32:34 +0900

    productモジュールの作成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductModule } from './product/product.module';
 
 const routes: Routes = [
-  { path: "", component: ProductListComponent },
-  { path: "detail", component: ProductDetailComponent },
-  // { path: "products/:productId", component: ProductDetailComponent },
-  // { path: "cart", component: CartComponent },
-  // { path: "shipping", component: ShippingComponent }
+  { path: "", redirectTo: "products", pathMatch: "full" },
+  // { path: "", component: ProductListComponent },
+  // { path: "detail", component: ProductDetailComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    ProductModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
