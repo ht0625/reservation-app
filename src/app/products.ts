c3f4c9fadda135d2805bb9ec262993d3390e188a
ht0625 <hiirookii0625@gmail.com>
2021-03-11 17:36:11 +0900

    -117

--- src/app/products.ts
+++ src/app/products.ts
@@ -10,7 +10,7 @@ export const products = [
 
   },
   {
-    coverImage: 'data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==',
+    coverImage: './assets/img/phone-cover.jpg',
     name: 'Phone Mini',
     price: 699,
     description: 'A great phone with one of the best cameras',
