a5f0397c1dccec6d4d88b5c45ff2e816ac10bff0
ht0625 <hiirookii0625@gmail.com>
2021-03-12 17:35:02 +0900

    -140

--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 // import { products } from '../../products';
 import { ProductService } from '../shared/product.service';
-
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-product-listings',
   templateUrl: './product-listings.component.html',
@@ -15,6 +15,24 @@ export class ProductListComponent implements OnInit {
 
   ngOnInit(): void {
     this.products = this.productService.getProducts()
+
+    const observable = new Observable(subscriber => {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(3);
+      setTimeout(() => {
+        subscriber.next(4);
+        subscriber.complete();
+      }, 1000);
+    });
+
+    console.log('just before subscribe');
+    observable.subscribe({
+      next(x) { console.log('got value ' + x); },
+      error(err) { console.error('something wrong occurred: ' + err); },
+      complete() { console.log('done'); }
+    });
+    console.log('just after subscribe');
   }
 
 }
