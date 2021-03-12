365bd7dfbeb4c7b684708b7b27e9ff272ccfb140
hiroki_tamura@gio-tech.jp <hiroki_tamura@gio-tech.jp>
2021-03-12 14:48:36 +0900

    -133

--- server/routes/products.js
+++ server/routes/products.js
@@ -7,7 +7,13 @@ router.get('', function(req, res){
   Product.find({},function(err, foundProducts){
     res.json(foundProducts)
   })
+})
 
+router.get('/:productId', function(req, res){
+  const productId = req.params.productId
+  Product.findById(productId,function(err, foundProduct){
+    res.json(foundProduct)
+  })
 })
 
 module.exports = router
