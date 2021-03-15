69e7a18e7551332e2eb5894072cfe945838fab27
hiroki_tamura@gio-tech.jp <hiroki_tamura@gio-tech.jp>
2021-03-15 16:55:34 +0900

    ログイン画面　2-4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductModule } from './product/product.module';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "products", pathMatch: "full" },
+  { path: "login", component: LoginComponent },
   // { path: "", component: ProductListComponent },
   // { path: "detail", component: ProductDetailComponent },
 ];
