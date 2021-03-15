9d5178712d70be5687417d3708fb59b44dc06de1
ht0625 <hiirookii0625@gmail.com>
2021-03-15 17:25:55 +0900

    2-8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
-import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
