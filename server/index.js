ff76d96854995ee8c2414dd31671db19a119cd57
ht0625 <hiirookii0625@gmail.com>
2021-03-16 11:55:56 +0900

    2-13

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const config = require('./config/dev')
 const FakeDb = require('./fake-db')
 
 const productRoutes = require('./routes/products')
+const userRoutes = require('./routes/users')
 
 
 // await mongoose.connect('mongodb+srv://test:testtest@cluster0.stqp1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
@@ -15,8 +17,9 @@ const productRoutes = require('./routes/products')
 mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
   // useFindAndModify: false,
-  // useCreateIndex: true
+
 }).then(
   () => {
     const fakeDb = new FakeDb()
@@ -25,8 +28,10 @@ mongoose.connect(config.DB_URI, {
 )
 
 const app = express()
+app.use(bodyParser.json())
 
 app.use('/api/v1/products', productRoutes)
+app.use('/api/v1/users', userRoutes)
 
 // app.get('/products', function(req, res){
 //   res.json({'success': true})
---
+++ server/routes/users.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+const Product = require('../model/user')
+
+
+router.post('/login', function(req, res){
+  // Product.find({},function(err, foundProducts){
+  //   return res.json(foundProducts)
+  // })
+})
+
+router.post('/register', function(req, res){
+  const username = req.body.username
+  const email = req.body.email
+  const password = req.body.password
+  // Product.findById(productId,function(err, foundProduct){
+  //   if(err){
+  //     return res.status(422).send({errors: [{title: 'Product error', detail: 'Product not found'}]})
+  //   }
+  //   return res.json(foundProduct)
+  return res.json({username, email , password})
+  // })
+})
+
+module.exports = router
