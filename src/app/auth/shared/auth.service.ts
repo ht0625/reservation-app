c007a7bec389ec69df8078d67bc6302410079fc7
hiroki_tamura@gio-tech.jp <hiroki_tamura@gio-tech.jp>
2021-03-17 15:44:37 +0900

    2-56

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -3,12 +3,22 @@ import { products } from '../../products';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators'
+import { JwtHelperService } from "@auth0/angular-jwt";
+
+const jwt = new JwtHelperService();
+
+class DecodedToken{
+  userId: string = ''
+  username: string = ''
+  exp: number = 0
+}
 
 @Injectable()
-
 export class AuthService{
-
-  constructor(private http: HttpClient) { }
+  private decodedToken
+  constructor(private http: HttpClient) {
+    this.decodedToken = JSON.parse(localStorage.getItem('app-meta')) || new DecodedToken()
+  }
 
   // getProducts(): Observable<any>{
   //   return this.http.get('/api/v1/products')
@@ -21,8 +31,9 @@ export class AuthService{
   login(userData: any): Observable<any>{
     return this.http.post('/api/v1/users/login', userData).pipe(map(
       (token: string) => {
-        debugger
+        this.decodedToken = jwt.decodeToken(token)
         localStorage.setItem('app-auth', token)
+        localStorage.setItem('app-meta', JSON.stringify(this.decodedToken))
         return token
       }
     ))
